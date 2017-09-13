4ca94b3806c1fc3feaa06fb9a3dba6467e1e1de3
Desislava Todorova <dslavatodorova@gmail.com>
2017-09-12 19:27:41 -0600

    update models associations spec

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 RSpec.describe Order, type: :model do
   describe "assocaitions" do
+    it {is_expected.to belong_to(:user)}
     it {is_expected.to have_many(:items)}
     it {is_expected.to have_many(:order_items)}
   end
